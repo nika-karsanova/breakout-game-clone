ef6e1441744ea4f9946b531e39e02678f5cc1d29
Nika Karsanova <51246589+nika-karsanova@users.noreply.github.com>
2022-05-25 00:30:01 +0100

    Add files via upload

---
+++ src/Assets/Scripts/Interfaces/IInteraction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IInteraction
+    {
+        /* Interface that Addresses the problem of different behaviours upon brick destructions.
+         Can be easily extended to accommodate for either more types of bricks or some other
+         collidable objects.
+         */
+        void ObjectInteraction();
+
+    }
+}
---
+++ src/Assets/Scripts/Interfaces/IPowerup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IPowerup
+    {
+        /* Interface that deals with the powerup application. Triggered by collision of a puck with brick
+         */
+        void ApplyPowerup(Collider2D collider=null);
+    }
+}
